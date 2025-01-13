af474f2f12734444edb0ab0e39bfa95ed986e28f
Vinay Komakula <vinaykomakula04@gmail.com>
2025-01-13 14:47:30 +0530

    Add files via upload

---
+++ recommend/app.py
@@ -0,0 +1,118 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+app = Flask(__name__)
+
+# load files
+trending_products = pd.read_csv("models/trending_products.csv")
+train_data = pd.read_csv("models/clean_data.csv")
+
+# Function to truncate product name
+def truncate(text, length):
+    if len(text) > length:
+        return text[:length] + "..."
+    else:
+        return text
+
+def content_based_recommendations(train_data, item_name, top_n=10):
+    # Check if the item name exists in the training data
+    if item_name not in train_data['Name'].values:
+        print(f"Item '{item_name}' not found in the training data.")
+        return pd.DataFrame()
+
+    # Create a TF-IDF vectorizer for item descriptions
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+
+    # Apply TF-IDF vectorization to item descriptions
+    tfidf_matrix_content = tfidf_vectorizer.fit_transform(train_data['Tags'])
+
+    # Calculate cosine similarity between items based on descriptions
+    cosine_similarities_content = cosine_similarity(tfidf_matrix_content, tfidf_matrix_content)
+
+    # Find the index of the item
+    item_index = train_data[train_data['Name'] == item_name].index[0]
+
+    # Get the cosine similarity scores for the item
+    similar_items = list(enumerate(cosine_similarities_content[item_index]))
+
+    # Sort similar items by similarity score in descending order
+    similar_items = sorted(similar_items, key=lambda x: x[1], reverse=True)
+
+    # Get the top N most similar items (excluding the item itself)
+    top_similar_items = similar_items[1:top_n+1]
+
+    # Get the indices of the top similar items
+    recommended_item_indices = [x[0] for x in top_similar_items]
+
+    # Get the details of the top similar items
+    recommended_items_details = train_data.iloc[recommended_item_indices][['Name', 'ReviewCount', 'Brand', 'ImageURL', 'Rating']]
+
+    return recommended_items_details
+
+# List of predefined image URLs
+random_image_urls = [
+    "static/img/img_1.jpeg",
+    "static/img/img_2.jpeg",
+    "static/img/img_3.jpeg",
+    "static/img/img_4.jpeg",
+    "static/img/img_5.jpeg",
+    "static/img/img_6.jpeg",
+    "static/img/img_7.jpeg",
+    "static/img/img_8.jpeg",
+]
+
+@app.route("/")
+def index():
+    price = [49999, 2299, 37999, 112099, 15999, 39999, 1069, 2000]
+    return render_template('index.html', 
+                           trending_products=trending_products.head(8),
+                           truncate=truncate,
+                           random_product_image_urls=random_image_urls,
+                           random_price=price)
+
+@app.route("/main")
+def main():
+    # Initialize with an empty DataFrame and no message
+    return render_template('main.html', 
+                           content_based_rec=pd.DataFrame(), 
+                           message=None)
+
+@app.route("/index")
+def indexredirect():
+    price = [49999, 2299, 37999, 112099, 15999, 39999, 1069, 2000]
+    return render_template('index.html', 
+                           trending_products=trending_products.head(8), 
+                           truncate=truncate,
+                           random_product_image_urls=random_image_urls,
+                           random_price=random.choice(price))
+
+@app.route("/recommendations", methods=['POST', 'GET'])
+def recommendations():
+    if request.method == 'POST':
+        prod = request.form.get('prod')
+        nbr = int(request.form.get('nbr'))
+        content_based_rec = content_based_recommendations(train_data, prod, top_n=nbr)
+
+        if content_based_rec.empty:
+            message = "No recommendations available for this product."
+            return render_template('main.html', 
+                                   message=message, 
+                                   content_based_rec=pd.DataFrame())
+        else:
+            # Create a list of random image URLs for each recommended product
+            random_product_image_urls = [random.choice(random_image_urls) for _ in range(len(content_based_rec))]
+            
+            price = [40, 50, 60, 70, 100, 122, 106, 50, 30, 50]
+            random_prices = [random.choice(price) for _ in range(len(content_based_rec))]
+            
+            return render_template('main.html', 
+                                   content_based_rec=content_based_rec, 
+                                   truncate=truncate,
+                                   random_product_image_urls=random_product_image_urls,
+                                   random_price=random_prices)
+
+if __name__=='__main__':
+    app.run(debug=True)
